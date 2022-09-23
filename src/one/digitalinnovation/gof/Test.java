120373be372d3a9c7a972dacea8b66e7c0300aaa
José Reynan <reynan.cavalcanti@gmail.com>
2022-09-23 18:18:51 -0300

    projetos em java

---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,42 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Test {
+    public static void main(String[] args) {
+
+        // Singleton
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonLazyHolder lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+        lazyholder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyholder);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        //Strategy
+        Comportamento agressivo = new ComportamentoAgressivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+
+        //Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Reynan","52314571");
+
+    }
+}
